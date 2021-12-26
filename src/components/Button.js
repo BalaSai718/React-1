bd6ea34acb97932c98660d8d4638cbb4823b3134
Bala Sai <balasai718@gmail.com>
2021-12-26 17:51:24 +0530

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,27 @@
 import React, { Component } from "react";
 
 export class Button extends Component {
+  state = {
+    visible: false,
+  };
 
   render() {
     return (
       <div>
-        <button id="click" >Click</button>
-        <p id="para">
-          Hello, I've learnt to use the full-stack evaluation tool. This makes
-          me so happy
-        </p>
+        <button
+          id="click"
+          onClick={() => {
+            this.setState({ visible: true });
+          }}
+        >
+          Click
+        </button>
+        {this.state.visible ? (
+          <p id="para">
+            Hello, I've learnt to use the full-stack evaluation tool. This makes
+            me so happy
+          </p>
+        ) : null}
       </div>
     );
   }
